cb32fbbd1156d2ea1687298dc0aa31b6d8140a7e
Klara Surmeier <k.surmeier@tu-braunschweig.de>
2026-04-24 15:00:35 +0200

    UVLFormulaFormatTest: deletion of test for ComputeEnumerateSolution

--- src/test/java/de/featjar/feature/model/io/UVLFormulaFormatTest.java
+++ src/test/java/de/featjar/feature/model/io/UVLFormulaFormatTest.java
@@ -156,109 +156,14 @@ public class UVLFormulaFormatTest extends Common {
                 new Equals(new Constant("Cherry"), new Constant("Cherry")),
                 new Implies(new And(new Literal("Arugula_def")), 
                 new Equals(new StringLength(new Variable("Arugula_val", String.class)), new Constant(7d)))));
+        
+//        Result<String> result = format.serialize(expectedFormula);
+//        System.out.println(result.get());
+
+        //if (result.isEmpty()) {
+        //    Assertions.fail();
+        //}
        
        Assertions.assertEquals(computedFormula.get(), expectedFormula);
-   }
-    
-   @Test
-   public void testEnumerateSolutionsWithMinimalSaladFeatureModel() {
-	   IFormula expectedFormula = new Reference(new And(new And(new And(new Literal("Veggies"), new Or(new Literal("Tomatoes"), new Literal("Cucumber"),
-   		    new Literal("Fennel"), new Literal("Beets")))), 
-   		    new Implies(new Literal("Fennel"), new And(new Literal("Beets"), new Not(new Literal("Cucumber")))), 
-   		    new GreaterEqual(new IntegerAdd(new Constant(90l), new Constant(100l)), new Constant(80d)),
-       		new LessThan(new IntegerMultiply(new Constant(80l), new Constant(100l)), new Constant(100000d)),
-            new Equals(new Constant(100l), new Constant(100d)), 
-            new BiImplies(new Literal("Beets"), new Or(new Literal("Cucumber"), new Not(new Literal("Tomatoes")))),
-            new LessEqual(new IntegerAdd(new Constant(100l), new IntegerMultiply(new Constant(-1l), new Constant(80l))), new Constant(30d)),
-            new GreaterThan(new IntegerDivide(new Constant(100l), new Constant(25l)), new Constant(3d)), 
-            new Equals(new Constant("Cherry"), new Constant("Cherry")),
-            new Implies(new And(new Literal("Arugula_def")), 
-            new Equals(new StringLength(new Variable("Arugula_val", String.class)), new Constant(7d)))));
-	   
-	   final Result<JavaSMTFormula> javaSMTFormulaResult =
-               Computations.of(expectedFormula)
-               .map(ComputeJavaSMTFormula::new)
-               .set(ComputeJavaSMTFormula.SOLVER, Solvers.Z3)
-               .computeResult();
-	   
-	   JavaSMTFormula javaSMTFormula = javaSMTFormulaResult.get();
-	   FormulaToJavaSMT formulaToJavaSmt = javaSMTFormula.getTranslator();
-	   VariableMap variableMap = javaSMTFormula.getVariableMap();
-	   
-	   formulaToJavaSmt.nodeToFormula(expectedFormula);
-	   FormulaManager formulaManager = formulaToJavaSmt.getCurrentFormulaManager();
-	   List<VariableReference> variableReferences = formulaToJavaSmt.getMappings();
-	   
-	   Result<List<List<BooleanFormula>>> booleanAssignmentsResult = Computations.of(javaSMTFormula)
-			   .map(ComputeSolutionEnumeration::new)
-			   .computeResult();
-	   
-	   List<List<BooleanFormula>> booleanAssignments = booleanAssignmentsResult.get();
-	   
-	   
-	   
-	   // define a BooleanFormulaVisitor which returns the index of the current atom
-       BooleanFormulaVisitor<Integer> booleanFormulaVisitor = (BooleanFormulaVisitor<Integer>) new DefaultBooleanFormulaVisitor<Integer>() {
-		   public Integer visitNot(BooleanFormula operand) {
-			   Variable variable = variableReferences.stream().filter(r -> r.getJavaSmtVariable().equals(operand))
-					   .map(r -> r.getVariable()).findFirst().orElse(null);
-			   
-			   Integer index = variableMap.get(variable.getName()).get();
-			  
-			   return -index;
-		   }
-		   
-		   public Integer visitAtom(BooleanFormula atom, FunctionDeclaration<BooleanFormula> funcDecl) {
-			   Variable variable = variableReferences.stream().filter(r -> r.getJavaSmtVariable().equals(atom))
-					   .map(r -> r.getVariable()).findFirst().orElse(null);
-			   
-			   Integer index = variableMap.get(variable.getName()).get();
-			   
-			   return index;
-		   }
-
-		   public Integer visitDefault() {
-			   return -1000;
-		   }
-	    };
-	    
-	    
-	    
-	    // create computed assignments
-	    List<BooleanAssignment> computedAssignments = new ArrayList<BooleanAssignment>();
-	    for (List<BooleanFormula> booleanAssignment : booleanAssignments) {
-	    	BooleanAssignment satisfyingAssignment = new BooleanAssignment();
-	    	for (BooleanFormula booleanVariable : booleanAssignment) {
-	    		Integer assignment = formulaManager.getBooleanFormulaManager()
-	    				.visit(booleanVariable, booleanFormulaVisitor);
-	    		satisfyingAssignment = satisfyingAssignment.addAll(assignment.intValue());
-	    	}
-	    	computedAssignments.add(satisfyingAssignment);
-	    }
-	
-	   
-	   // create expected assignments
-	   List<BooleanAssignment> expectedAssignments = new ArrayList<BooleanAssignment>();
-	   BooleanAssignment satisfyingAssignment1 = new BooleanAssignment(1, 2, -3, -4, -5, -6);
-	   BooleanAssignment satisfyingAssignment2 = new BooleanAssignment(1, -2, -4, 5, -6);
-	   BooleanAssignment satisfyingAssignment3 = new BooleanAssignment(1, -2, -3, 4, 5, -6);
-	   BooleanAssignment satisfyingAssignment4 = new BooleanAssignment(1, 2, 3, -4, 5, -6);
-	   BooleanAssignment satisfyingAssignment5 = new BooleanAssignment(1, 2, -3, -4, -5, 6);
-	   BooleanAssignment satisfyingAssignment6 = new BooleanAssignment(1, -2, -4, 5, 6);
-	   BooleanAssignment satisfyingAssignment7 = new BooleanAssignment(1, 2, 3, -4, 5, 6);
-	   BooleanAssignment satisfyingAssignment8 = new BooleanAssignment(1, -2, -3, 4, 5, 6);
-	   
-	   expectedAssignments.add(satisfyingAssignment1);
-	   expectedAssignments.add(satisfyingAssignment2);
-	   expectedAssignments.add(satisfyingAssignment3);
-	   expectedAssignments.add(satisfyingAssignment4);
-	   expectedAssignments.add(satisfyingAssignment5);
-	   expectedAssignments.add(satisfyingAssignment6);
-	   expectedAssignments.add(satisfyingAssignment7);
-	   expectedAssignments.add(satisfyingAssignment8);
-	  
-	   
-	   Assertions.assertEquals(computedAssignments, expectedAssignments); 
-   }
-	   
+   }   
 }
